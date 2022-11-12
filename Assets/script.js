3f6b299a0ae363ef6e2576b6d7009e35c1ec7f91
TuaTuivai <TuaMTuivai@gmail.com>
2022-11-11 18:34:19 -0800

    added local storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,20 @@
-var pastBtn        = document.querySelector('#pastBtn')
-var presentBtn     = document.querySelector('#presentBtn')
-var futureBtn      = document.querySelector('#futureBtn')
-
-
-
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var saveBtn = document.querySelector(".btn saveBtn col-2 col-md-1");
+var count   = localStorage.getItem('count');
+
+
+
+
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
+  saveBtn.addEventListener('click', function(){
+
+  });
+  // TODO: Add a listener for click events on the save button. 
+  
+  // This code should use the id in the containing time-block as a key to save the user input in
   // local storage.
   // HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -28,12 +33,3 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
-pastBtn.addEventListener('click', function(){
-    
-}) 
-presentBtn.addEventListener('click', function(){
-    
-}) 
-futureBtn.addEventListener('click', function(){
-    
-}) 
